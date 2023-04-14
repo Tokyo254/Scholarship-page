ab0f6d9471d899131cd84071923608511b6dd620
Tokyo254 <119660460+Tokyo254@users.noreply.github.com>
2023-04-14 07:49:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector(".nav-bar");
+
+hamburger.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+ 
+}
+);
+
